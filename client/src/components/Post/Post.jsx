386042c6ca75c1272870d2f5c196a368764eaf3f
jvsnyder <forexsnyder@gmail.com>
2020-08-17 13:04:33 -0700

    added clien/src/post and css

---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Post.css';
+import { Link } from 'react-router-dom'
+
+const Post = (props) => {
+    return (
+        <>
+        <Link className="post" to={`/posts/${props._id}`}>
+                <div className="post-title">{props.title}</div>
+                <div className="post-author">{props.author}</div>
+                <img className="post-image" src={props.imgURL} alt={props.name} />
+                <div className="post-description">{props.description}</div>
+                
+                {/* <div className="price">{`$${props.price}`}</div> */}
+            </Link>
+        </>
+    )
+}
+export default Post

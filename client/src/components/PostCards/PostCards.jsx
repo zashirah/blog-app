29b268ac8297100c6c728888dfc126acb9289a18
Zach Shirah <zachshirah@MacBook-Pro.attlocal.net>
2020-08-18 16:01:51 -0400

    fixed picture slider

--- client/src/components/PostCards/PostCards.jsx
+++ client/src/components/PostCards/PostCards.jsx
@@ -1,17 +1,40 @@
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PostCard from '../PostCard/PostCard'
 import './PostCards.css';
 import { Link } from 'react-router-dom'
+import { getPosts } from '../../services/posts'
 
 const PostCards = (props) => {
-    return (
-        <div className="post-card">
-            <Link className="card" to={`/posts/${props._id}`}>
-                <img className="post-card-image" src={props.imgURL} alt={props.name} />
-                <div>View</div>
-            </Link>
-        </div>
-    )
+  const [allPosts, updatePosts] = useState([])
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const posts = await getPosts();
+      updatePosts(posts);
+    };
+    fetchPost();
+  }, []);
+
+  const CARDS = allPosts.map((post, index) => 
+    index < 8 ? <PostCard 
+      _id={post._id}
+      title={post.title}
+      author={post.author}
+      imgURL={post.imgURL}
+      description={post.description}
+      key={index}
+      /> : null
+  )
+
+
+  return (
+    <div className="post-cards">
+      <div className="latest">LATEST</div>
+      <div className="cards">
+        {CARDS}
+      </div>
+    </div>
+  )
 }
 
 export default PostCards
